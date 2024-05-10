7d232bb64ff2076095ab35bc061dd55702ca64b7
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-05-11 02:41:07 +0700

    chore(tailwind): add extends colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ export default {
         'geist-semibold': ["Geist-SemiBold"],
         'geist-bold': ["Geist-Bold"]
       },
+      colors: {
+        'kmr-black': {
+          100: '#B9B9B9',
+          500: '#0A0A0A',
+          900: '#000000'
+        },
+        'kmr-green': {
+          0: '#408A4C'
+        }
+      }
     },
   },
   plugins: [],

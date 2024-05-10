d934c21e6f74eda1a7d0c83caf05a43ad21af704
azrappp <118091170+azrappp@users.noreply.github.com>
2024-05-10 23:22:59 +0700

    initial commit 🚀

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(params) {
+  return (
+    <>
+      <div className="text-center text-2xl text-green-500 font-geist">
+        <div>Kumar</div>
+        <div className="section">
+          <a href="">Home</a>
+          <a href="">Team</a>
+          <a href="">Contact</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;

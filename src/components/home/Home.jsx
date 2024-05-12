6e35238d37636275fa8e6b7a3c45328f272d3652
azrappp <118091170+azrappp@users.noreply.github.com>
2024-05-12 17:42:34 +0700

    feat: add footer and fixed font.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,20 @@
-import ImageHome from "../images/Imagehome";
 function Home(params) {
   return (
     <div className="container mx-auto max-w-[953px] pt-40">
       <div className="text-white flex flex-col items-center gap-8">
-        <div className="font-geist-bold text-center text-6xl">Hello World</div>
-        <div className="font-geist-reguler text-center text-2xl ">
+        <div className="font-geist font-bold text-center text-6xl">
+          Hello World
+        </div>
+        <div className="font-geist text-center text-2xl text-kmr-black-100">
           "Meet our exceptional team ✨ - each member brings expertise and
           passion, driving project success through collaboration and
           innovation."
         </div>
-        <button className="mt-8 mb-12 px-8 bg-white py-2.5 text-black font-geist-semibold  text-base rounded-full">
+        <button className="mt-8 mb-12 px-8 bg-white py-2.5 text-black font-geist font-bold text-base rounded-full">
           Get to Know
         </button>
         <div>
-          <ImageHome></ImageHome>
+          <img className="object-cover  " src="/images/ubuntunew.svg" alt="" />
         </div>
       </div>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,18 +3,15 @@ function Navbar(params) {
     <>
       <nav className="border-b border-white border-opacity-10 ">
         <div className="flex flex-row items-center justify-between text-center px-32 py-4">
-          <div className="text-white font-geist-bold text-2xl">Kumar</div>
+          <div className="text-white font-geist font-bold text-2xl">Kumar</div>
           <div className="text-kmr-grey-200 flex gap-6">
-            <a className=" font-geist-regular text-sm hover:text-white" href="">
+            <a className=" font-geist text-sm hover:text-white" href="">
               Home
             </a>
-            <a
-              className=" font-geist-regular text-sm  hover:text-white"
-              href=""
-            >
+            <a className=" font-geist  text-sm  hover:text-white" href="">
               Team
             </a>
-            <a className=" font-geist-regular text-sm hover:text-white" href="">
+            <a className=" font-geist  text-sm hover:text-white" href="">
               Contact
             </a>
           </div>

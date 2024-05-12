365ec01a9994bda0b404a82a47302acccf5d47d0
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-05-12 15:17:12 +0700

    feat: add team-page 'interests' content-box

---
+++ src/components/team-content-box/InterestContentBox.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+const InterestBox = ({interest}) => {
+    return (
+        <div className="flex h-8 px-[14px] py-2 justify-center items-center bg-white bg-opacity-5 border border-white border-opacity-10 rounded-md backdrop-blur-lg">
+            <p className="font-mono text-[12px] text-white">{interest}</p>
+        </div>
+    )
+}
+
+const InterestContentBox = ({interests}) => {
+    return (
+        <div className="flex flex-col gap-6 px-8 py-6">
+            <div className="flex gap-3">
+                <img src="/icons/interest.svg"></img>
+                <h1 className="font-geist-semibold text-white">Interest</h1>
+            </div>
+            <div className="flex gap-3">
+                {
+                    interests.map((interest) => InterestBox({interest}))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default InterestContentBox;

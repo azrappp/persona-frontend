82d156f5de3c5acd123d31ae092b6817f55628b6
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-05-21 09:32:03 +0700

    feat: add spotlight element on hero section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <header className="fixed top-0 w-full z-50">
-      <nav className="border-b border-white border-opacity-10 bg-black w-full">
+      <nav className="border-b border-white border-opacity-10 bg-black backdrop-blur-lg bg-opacity-10 w-full">
         <div className="flex flex-row items-center justify-between text-center px-8 md:px-32 py-4">
           <div className="text-white font-geist font-bold text-2xl w-20">Kumar</div>
           <div className="hidden text-kmr-grey-200 md:flex gap-6 pr-20 flex-1 justify-center">
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import Footer from "../components/footer/Footer";
 import Home from "../components/home/Home";
 import Navbar from "../components/navbar/Navbar";
 import Team from "../components/team/Team";
+import Spotlight from "../components/ui/Spotlight"
 
 const LandingPage = () => {
   return (
@@ -10,6 +11,20 @@ const LandingPage = () => {
       <div className="py-48 px-8 md:px-16 lg:px-32">
         <Home />
         <Team />
+        <div className="">
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
+          />
+          <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+          />
+          <Spotlight 
+            className="left-80 top-28 h-[80vh] w-[50vw]" 
+            fill="blue" 
+          />
+        </div>
       </div>
       <Footer />
     </>
